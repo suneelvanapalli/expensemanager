4999375268baad6d1052d177dccd47f1f6522712
SuneelKumar Vanapalli <suneel.vanapalli@adudev.com>
2019-04-27 18:21:51 +1000

    graphiql initi changes

--- em-node-api/src/index.ts
+++ em-node-api/src/index.ts
@@ -1,7 +1,21 @@
 import express from 'express';
 import { MongoClient, MongoClientOptions } from 'mongodb';
+import  { buildSchema } from 'graphql';
+import graphqlHttp = require('express-graphql');
 
 const url = "mongodb://localhost:27017/test";
+
+const schema = buildSchema(`
+   type Query{
+       hello: string
+   }
+`);
+
+let root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+  };
 
 MongoClient.connect(url, (err,db) => {
     var dbo = db.db("admin");
@@ -12,6 +26,8 @@ MongoClient.connect(url, (err,db) => {
 });
 
 const app = express();
+
+app.use('/graphql', graphqlHttp({ schema: schema, rootValue: root, graphiql : true  }));
 
 const port = 3000;
 const serverName = 'test';
